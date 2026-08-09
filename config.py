0bef3d448e86a12999869c13ae5edeae3add1a50
cycgcyvyhgugufug-coder <cycgcyvyhgugufug@gmail.com>
2026-08-09 21:52:36 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8921125294:AAGVGELVKXk9k5Xjsuor5mVlkfUUB7XuTSU")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "6237680057"))
