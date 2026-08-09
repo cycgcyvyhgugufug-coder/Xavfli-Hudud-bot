1cffa32806c5072f21ed70b7efedebfd23e96d64
cycgcyvyhgugufug-coder <cycgcyvyhgugufug@gmail.com>
2026-08-09 21:55:28 +0500

    Create states.py

---
+++ states.py
@@ -0,0 +1,23 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class AdminAddVideo(StatesGroup):
+    waiting_for_cover = State()
+    waiting_for_post_desc = State()
+    waiting_for_main_desc = State()
+    waiting_for_price = State()
+    waiting_for_quality_select = State()
+    waiting_for_video_file = State()
+    waiting_for_code = State()
+
+class AdminDeleteVideo(StatesGroup):
+    waiting_for_code = State()
+
+class AdminSettings(StatesGroup):
+    waiting_for_base_channel = State()
+    waiting_for_mandatory_channel = State()
+    waiting_for_card_number = State()
+    waiting_for_admin_link = State()
+
+class UserPayment(StatesGroup):
+    waiting_for_receipt_vip = State()
+    waiting_for_receipt_video = State()
